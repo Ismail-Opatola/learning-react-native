02b4b9fb8be24a6a565483e84080e08132385276
Ismail Opatola <opatolamails@gmail.com>
2021-04-25 22:43:15 +0100

    code refactor import restaurant screen

--- mealsToGo/App.js
+++ mealsToGo/App.js
@@ -1,51 +1,12 @@
 import { StatusBar as ExpoStatusBar } from "expo-status-bar";
 import React from "react";
-import {
-  StyleSheet,
-  Text,
-  View,
-  SafeAreaView,
-  // Platform,
-  StatusBar,
-} from "react-native";
-
-// const isAndroid = Platform.OS === "android";
+import { RestaurantScreen } from "./src/features/restaurants/components/restaurants.screen";
 
 export default function App() {
   return (
     <>
-      <SafeAreaView style={styles.container}>
-        <View style={styles.innerContainer}>
-          <View styles={styles.search}>
-            <Text>search</Text>
-          </View>
-          <View styles={styles.list}>
-            <Text>list</Text>
-          </View>
-        </View>
-      </SafeAreaView>
-      <ExpoStatusBar />
+      <RestaurantScreen />
+      <ExpoStatusBar style="auto" />
     </>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    marginTop: StatusBar.currentHeight,
-  },
-  innerContainer: {
-    flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
-  },
-  search: {
-    padding: 16,
-    backgroundColor: "green",
-  },
-  list: {
-    flex: 1,
-    padding: 16,
-    backgroundColor: "blue",
-  },
-});
