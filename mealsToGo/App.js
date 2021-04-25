7c902723906a07b1613e39b6c41b014150eb44a5
Ismail Opatola <opatolamails@gmail.com>
2021-04-26 00:08:04 +0100

    themeimg with styled-components create external theme import ThemeProvider pass theme to ThemeProvider Context

--- mealsToGo/App.js
+++ mealsToGo/App.js
@@ -1,12 +1,16 @@
 import { StatusBar as ExpoStatusBar } from "expo-status-bar";
 import React from "react";
+import { ThemeProvider } from "styled-components/native";
 import { RestaurantScreen } from "./src/features/restaurants/components/restaurants.screen";
+import { theme } from "./src/infrastructure/theme";
 
 export default function App() {
   return (
     <>
-      <RestaurantScreen />
-      <ExpoStatusBar style="auto" />
+      <ThemeProvider theme={theme}>
+        <RestaurantScreen />
+        <ExpoStatusBar style="auto" />
+      </ThemeProvider>
     </>
   );
 }
